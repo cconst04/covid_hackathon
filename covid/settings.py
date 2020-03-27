6d4e18c2207c64fee8267caea841669c07468474
cconst04 <cconst04@cs.ucy.ac.cy>
2020-03-27 12:25:38 +0000

    Implemented file upload

--- covid/settings.py
+++ covid/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+CSV_FILENAME = 'data.csv'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Full filesystem path to the project.
@@ -23,6 +24,10 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
   os.path.join(SITE_ROOT, 'static/'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
--- graph/views.py
+++ graph/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from os.path import exists,join
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import csv
 #templates/examples/dashboard/index.html
 def index(request):
-    return render(request,'examples/dashboard/index.html')
-    # return HttpResponse("Hello, world. You're at the polls index.")
+    if exists(join(settings.MEDIA_ROOT,settings.CSV_FILENAME)):
+        # import pdb;pdb.set_trace()
+        # test = pd.read_csv(join(settings.MEDIA_ROOT,settings.CSV_FILENAME))
+        with open(join(settings.MEDIA_ROOT,settings.CSV_FILENAME), newline='') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+        return render(request,'index.html',{'data':data})
+    elif request.method == 'POST' and 'myfile' in request.FILES and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(settings.CSV_FILENAME, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request,'index.html')
+    return render(request,'upload.html')
