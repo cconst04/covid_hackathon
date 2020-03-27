a234600846f2bccc4ffac3dee648b4394ed9fb4f
Chris L <44942864+lkrist01@users.noreply.github.com>
2020-03-27 22:48:58 +0200

    Add files via upload
    Added events

--- Event_generator/data.py
+++ Event_generator/data.py
@@ -4,13 +4,18 @@ import random
 import pandas as pd
 
 
-#data = pd.read_csv("Larnaca_out.csv")
-#data = pd.read_csv("Limassol_out.csv")
-#data = pd.read_csv("Nicosia_out.csv")
-data = pd.read_csv("Paphos.csv")
+data1 = pd.read_csv("Larnaca_out.csv")
+data2 = pd.read_csv("Limassol_out.csv")
+data3 = pd.read_csv("Nicosia_out.csv")
+data4 = pd.read_csv("Paphos_out.csv")
 
-print(data.post_code)
+# print(data1.post_code)
+# print(data2.post_code)
+# print(data3.post_code)
+# print(data4.post_code)
 
+
+print()
 fake = Faker()
 
 
@@ -18,13 +23,46 @@ records=1000
 print("Making %d records\n" % records)
 
 fieldnames=['id','reason','timestamp','postal_code']
-writer = csv.DictWriter(open("Paphos.csv", "w",newline=''), fieldnames=fieldnames)
+writer1 = csv.DictWriter(open("Larnaca.csv", "w",newline=''), fieldnames=fieldnames)
+writer2 = csv.DictWriter(open("Limassol.csv", "w",newline=''), fieldnames=fieldnames)
+writer3 = csv.DictWriter(open("Nicosia.csv", "w",newline=''), fieldnames=fieldnames)
+writer4 = csv.DictWriter(open("Paphos.csv", "w",newline=''), fieldnames=fieldnames)
 
-writer.writerow(dict(zip(fieldnames, fieldnames)))
+
+writer1.writerow(dict(zip(fieldnames, fieldnames)))
 for i in range(0, records):
-    pos=random.randint(0,24)
-    writer.writerow(dict([
+    pos=random.randint(0,len(data1.index)-1)
+    writer1.writerow(dict([
     ('id', str(random.randint(1000,100000))),
     ('reason', str(random.randint(1,8))),
-    ('timestamp', fake.date_time_between(start_date='-30y', end_date='now', tzinfo=None)),
-    ('postal_code', str(data.post_code[pos]))]))
+    ('timestamp', fake.date_time_between(start_date='-1m', end_date='now', tzinfo=None)),
+    ('postal_code', str(data1.post_code[pos]))]))
+
+writer2.writerow(dict(zip(fieldnames, fieldnames)))
+for i in range(0, records):
+    pos=random.randint(0,len(data2.index)-1)
+    writer2.writerow(dict([
+    ('id', str(random.randint(1000,100000))),
+    ('reason', str(random.randint(1,8))),
+    ('timestamp', fake.date_time_between(start_date='-1m', end_date='now', tzinfo=None)),
+    ('postal_code', str(data2.post_code[pos]))]))
+
+
+writer3.writerow(dict(zip(fieldnames, fieldnames)))
+for i in range(0, records):
+    pos=random.randint(0,len(data3.index)-1)
+    writer3.writerow(dict([
+    ('id', str(random.randint(1000,100000))),
+    ('reason', str(random.randint(1,8))),
+    ('timestamp', fake.date_time_between(start_date='-1m', end_date='now', tzinfo=None)),
+    ('postal_code', str(data3.post_code[pos]))]))
+
+
+writer4.writerow(dict(zip(fieldnames, fieldnames)))
+for i in range(0, records):
+    pos=random.randint(0,len(data4.index)-1)
+    writer4.writerow(dict([
+    ('id', str(random.randint(1000,100000))),
+    ('reason', str(random.randint(1,8))),
+    ('timestamp', fake.date_time_between(start_date='-1m', end_date='now', tzinfo=None)),
+    ('postal_code', str(data4.post_code[pos]))]))
